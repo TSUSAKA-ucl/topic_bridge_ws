d21af5a94f066532098b2b15baa3e0287bb06b53
Yuji Tsusaka <tsusaka@ucl.nuee.nagoya-u.ac.jp>
2025-08-01 16:16:41 +0900

    python版のweb socketサーバーを追加。httpsに合わせてwssで動くようにする

--- runme.sh
+++ runme.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/bash
-node pose_bridge.js
+node pose_bridge.js --ros-args -r left_controller_pose:=vr_pose
--- server.sh
+++ server.sh
@@ -1,1 +1,30 @@
-python3 -m http.server 8080
+#!/usr/bin/env python3
+#
+#!/bin/bash
+# python3 -m http.server 8080
+#
+# https_server.py
+import os
+import http.server
+import ssl
+
+HOST = 'localhost'
+PORT = 4443
+
+# 任意のディレクトリをルートにする
+Handler = http.server.SimpleHTTPRequestHandler
+
+httpd = http.server.HTTPServer((HOST, PORT), Handler)
+
+basedir = os.path.dirname(__file__)  # this directory
+certfile = os.path.join(basedir, "..", "certificates", "localhost.pem")
+keyfile = os.path.join(basedir, "..", "certificates", "localhost-key.pem")
+
+context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+context.load_cert_chain(certfile=certfile, keyfile=keyfile)
+# 証明書と秘密鍵を読み込んで SSL 化
+httpd.socket = context.wrap_socket(httpd.socket,
+				   server_side=True,)
+
+print(f"Serving HTTPS on https://{HOST}:{PORT}")
+httpd.serve_forever()
